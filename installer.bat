53c708c775630506ee0c15169ca3efc919118b8b
Nguyen Duc Toan <ntoan96@gmail.com>
2025-10-08 14:11:49 +0700

    update installer.bat

--- installer.bat
+++ installer.bat
@@ -5,21 +5,21 @@ if %ERRORLEVEL% neq 0 (
     powershell -ExecutionPolicy Bypass -c "irm https://github.com/astral-sh/uv/releases/download/0.8.24/uv-installer.ps1 | iex"
 )
 
-%USERPROFILE%/.local/bin/uv.exe tool install https://github.com/ndtoan96/analytik/releases/download/v0.1.0/analytik-0.1.0-py3-none-any.whl
+%USERPROFILE%\.local\bin\uv.exe tool install https://github.com/ndtoan96/analytik/releases/download/v0.1.0/analytik-0.1.0-py3-none-any.whl
 if %ERRORLEVEL% neq 0 (
     echo Failed to install analytik. Exiting.
     pause
     exit /b 1
 )
 
-%USERPROFILE%/.local/bin/uv.exe tool install playwright
+%USERPROFILE%\.local\bin\uv.exe tool install playwright
 if %ERRORLEVEL% neq 0 (
     echo Failed to install Playwright. Exiting.
     pause
     exit /b 1
 )
 
-%USERPROFILE%/.local/bin/uv.exe tool run playwright install chromium
+%USERPROFILE%\.local\bin\uv.exe tool run playwright install chromium
 if %ERRORLEVEL% neq 0 (
     echo Failed to install Chromium for Playwright. Exiting.
     pause
@@ -27,7 +27,7 @@ if %ERRORLEVEL% neq 0 (
 )
 
 echo @echo off > analytik.bat
-echo %USERPROFILE%/.local/bin/uv.exe tool run analytik >> analytik.bat
+echo %USERPROFILE%\.local\bin\uv.exe tool run analytik >> analytik.bat
 
 echo Installation complete, press any key to close
 pause
